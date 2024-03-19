f6ef1e7ed4e71aec00d5cf6ca80235c91dad2bec
TridipChavda <chavdatridip007@gmail.com>
2024-03-19 16:18:27 +0530

    Task 11 with csv To MongoDB and Paging

---
+++ Task 11 csvToMongoDB/React_Frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TrainPage from './Components/train';
+
+function App() {
+  return (
+    <div className="App">
+      <TrainPage/>
+    </div>
+  );
+}
+
+export default App;
